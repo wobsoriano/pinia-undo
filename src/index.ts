dfaa7bb6825ccb684f9fee93eaaee03240d140cb
wobsoriano <sorianorobertc@gmail.com>
2023-12-04 14:25:22 -0800

    feat: add custom serializer option

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,27 @@
 import type { PiniaPluginContext } from 'pinia'
-import structuredClone from '@ungap/structured-clone'
 import createStack from 'undo-stacker'
-import { toRaw } from 'vue'
 
 type Store = PiniaPluginContext['store']
 type Options = PiniaPluginContext['options']
+
+interface Serializer {
+  serialize: (value: any) => string
+  deserialize: (value: string) => any
+}
 
 /**
  * Removes properties from the store state.
  * @param options The options object defining the store passed to `defineStore()`.
  * @param store The store the plugin is augmenting.
+ * @param serializer Customer serializer to serialize state before storing it in the undo stack.
  * @returns {object} State of the store without omitted keys.
  */
-function removeOmittedKeys(options: Options, store: Store): Store['$state'] {
-  const clone = structuredClone(toRaw(store.$state))
+function removeOmittedKeys(
+  options: Options,
+  store: Store,
+  serializer: Serializer = { serialize: JSON.stringify, deserialize: JSON.parse },
+): Store['$state'] {
+  const clone = serializer.deserialize(serializer.serialize(store.$state))
   if (options.undo && options.undo.omit) {
     options.undo.omit.forEach((key) => {
       delete clone[key]
@@ -21,6 +29,13 @@ function removeOmittedKeys(options: Options, store: Store): Store['$state'] {
     return clone
   }
   return clone
+}
+
+type PluginOptions = PiniaPluginContext & {
+  /**
+   * Customer serializer to serialize state before storing it in the undo stack.
+   */
+  serializer?: Serializer
 }
 
 /**
@@ -35,10 +50,10 @@ function removeOmittedKeys(options: Options, store: Store): Store['$state'] {
  * pinia.use(PiniaUndo)
  * ```
  */
-export function PiniaUndo({ store, options }: PiniaPluginContext) {
+export function PiniaUndo({ store, options, serializer }: PluginOptions) {
   if (options.undo && options.undo.disable)
     return
-  const stack = createStack(removeOmittedKeys(options, store))
+  const stack = createStack(removeOmittedKeys(options, store, serializer))
   let preventUpdateOnSubscribe = false
   store.undo = () => {
     preventUpdateOnSubscribe = true
